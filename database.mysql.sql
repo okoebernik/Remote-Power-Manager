4a22622b89270721d01453fe2a1b5382d8a022df
okoebernik <51490484+okoebernik@users.noreply.github.com>
2026-06-30 10:55:32 +0200

    Initial project import

---
+++ database.mysql.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS remote_power_manager
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE remote_power_manager;
+
+-- Die Tabellen werden von der Anwendung automatisch angelegt.
